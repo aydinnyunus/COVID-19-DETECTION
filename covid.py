58a6c6a166b8c5879d59a82569ec36819df527cb
Yunus AYDIN <aydinnyunus@gmail.com>
2020-03-17 01:40:59 +0300

    Update covid.py

--- covid.py
+++ covid.py
@@ -34,69 +34,44 @@ def parse_prediction_array(prediction):
         return {"label": "Normal", "p": probability_to_percentage(prediction[0][max_index])}
 
 
-# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-                help="path to input dataset")
-ap.add_argument("-m", "--model", type=str, default="Corona.h5",
-                help="path to output loss/accuracy plot")
+ap.add_argument("-d", "--data", required=True,
+                help="Path of your Dataset")
 args = vars(ap.parse_args())
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
 INIT_LR = 1e-3
 EPOCHS = 25
 BS = 8
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
-print("[INFO] loading images...")
 imagePaths = list(paths.list_images(args["dataset"]))
 data = []
 labels = []
-
-# loop over the image paths
 for imagePath in imagePaths:
-    # extract the class label from the filename
     label = imagePath.split(os.path.sep)[-2]
-
-    # load the image, swap color channels, and resize it to be a fixed
-    # 224x224 pixels while ignoring aspect ratio
     image = cv2.imread(imagePath)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (224, 224))
 
-    # update the data and labels lists, respectively
     data.append(image)
     labels.append(label)
 
-# convert the data and labels to NumPy arrays while scaling the pixel
-# intensities to the range [0, 255]
 data = np.array(data) / 255.0
 labels = np.array(labels)
 
-# perform one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
-# partition the data into training and testing splits using 80% of
-# the data for training and the remaining 20% for testing
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
                                                   test_size=0.20, stratify=labels, random_state=42)
 
-# initialize the training data augmentation object
 trainAug = ImageDataGenerator(
     rotation_range=15,
     fill_mode="nearest")
 
-# load the VGG16 network, ensuring the head FC layer sets are left
-# off
 baseModel = VGG16(weights="imagenet", include_top=False,
                   input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(4, 4))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -104,23 +79,15 @@ headModel = Dense(64, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
 for layer in baseModel.layers:
     layer.trainable = False
 
-# compile our model
-print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
               metrics=["accuracy"])
 
-# train the head of the network
-print("[INFO] training head...")
 H = model.fit_generator(
     trainAug.flow(trainX, trainY, batch_size=BS),
     steps_per_epoch=len(trainX) // BS,
@@ -128,29 +95,21 @@ H = model.fit_generator(
     validation_steps=len(testX) // BS,
     epochs=EPOCHS)
 
-# make predictions on the testing set
-print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
                             target_names=lb.classes_))
 
-# compute the confusion matrix and and use it to derive the raw
-# accuracy, sensitivity, and specificity
 cm = confusion_matrix(testY.argmax(axis=1), predIdxs)
 total = sum(sum(cm))
-acc = (cm[0, 0] + cm[1, 1]) / total
+accuracy = (cm[0, 0] + cm[1, 1]) / total
 
-# show the confusion matrix, accuracy, sensitivity, and specificity
-print("acc: {:.4f}".format(acc))
-model.save(args["model"], save_format="h5")
+print("Accuracy Score : {:.4f}".format(accuracy))
+model.save("Corona.h5", save_format="h5")
 
-img_src = '/home/aydinnyunus/Desktop/test.jpg'
+img_src = 'ENTER YOUR TEST IMAGE'
 img = cv2.imread(img_src)
 
 target_width = 224
@@ -159,7 +118,7 @@ target_size = (target_width, target_height)
 
 img = cv2.resize(img, target_size)
 img = img.reshape(1, target_width, target_height, 3)
-loaded_model = load_model('/home/aydinnyunus/PycharmProjects/Covid/covid19.model')
+loaded_model = load_model('Corona.h5')
 
 prediction = loaded_model.predict(img)
 
